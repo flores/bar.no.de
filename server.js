15df5de50e3fe89649ead5d81349982ac0ae4eed
flores <github@petalphile.com>
2012-01-06 17:09:12 -0800

    some ugly tz shit

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ app.get('/', function(req, res){
   var now  = new Date();
   var hour = now.getHours() - 8;
   var day  = now.getDay();
-  if ( ( hour >= 16 || ( hour > -7 && hour < 2 ) ) && day < 7 ) {
+  if ( hour >= 16 && day < 7 ) {
     res.send('Barcade is open! Time to drink.');
   }
   else if ( hour >= 12 && day == 7 ) {
     res.send('Barcade opens early on Sundays!  Go drink and be with God.');
   } 
-  else if ( hour <= 1 ) {
-    res.send('Barcade is open, but is about to close!!');
+  else if ( hour <= 1 && hour > -7 ) {
+    res.send('Barcade is open!  Time to drink.');
   }
   else {
     res.send('Barcade is closed.  You should drink to make yourself feel better.');
