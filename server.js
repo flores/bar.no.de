526cb8d43a089be290e67b95e945ce0f8b8a2e40
flores <github@petalphile.com>
2012-01-06 18:27:24 -0800

    getDay is 0 indexed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
   if ( hour >= 16 && day < 7 ) {
     res.send('Barcade is open! Time to drink.');
   }
-  else if ( hour >= 12 && day == 7 ) {
+  else if ( hour >= 12 && day == 6 ) {
     res.send('Barcade opens early on Sundays!  Go drink and be with God.');
   } 
   else if ( hour <= -6 && hour >= -7 ) {
