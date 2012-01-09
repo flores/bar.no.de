9fd4c7324c967192a466d330dc0787418b2538aa
flores <github@petalphile.com>
2012-01-09 15:27:55 -0800

    bug fixes

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var app = require('express').createServer();
+app.register('.html', require('jade'));
 
 function checkOpen() {
   var now  = new Date();
   // UTC
   var hour = now.getHours();
   var day  = now.getDay();
-  if ( hour >= 19 && day == 0 ) {
+  if ( hour >= 20 && day == 0 ) {
     var message = 'Blipsy Barcade opens early on Sundays!  Go and drink!';
   }
-  else if ( hour < 8 && day == 1 ) { 
+  else if ( hour < 9 && day == 1 ) { 
     var message = 'Dude, Blipsy has been open since noon.  Go and drink!';
   }
-  else if ( ( hour >= 23 || hour < 8 ) && day > 0 ) {
+  else if ( ( hour >= 0 || hour < 9 ) && day > 0 ) {
     var message = 'Barcade is open! Time to drink.';
   }
-  else if ( hour < 9 && hour >= 8 ) {
+  else if ( hour < 10 && hour >= 9 ) {
     var message = 'Barcade is about to close, fool.  Hurry up!';
   }
   else {
@@ -25,7 +26,7 @@ function checkOpen() {
 };
 
 app.get('/', function(req, res){
-  res.render('index', { message: checkOpen() })
+  res.render('index.html', { message: checkOpen() })
 });
 
 app.listen(process.env.PORT || 8001);
