db1d205ec3ca1ba8f2e3ea9e848380ea509c6e25
flores <github@petalphile.com>
2012-01-08 19:07:03 -0800

    add views

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 var app = require('express').createServer();
 
-app.get('/', function(req, res){
+function checkOpen() {
   var now  = new Date();
   // UTC
   var hour = now.getHours();
   var day  = now.getDay();
   if ( hour >= 19 && day == 0 ) {
-    res.send('Blipsy Barcade opens early on Sundays!  Go and drink!');
+    var message = 'Blipsy Barcade opens early on Sundays!  Go and drink!';
   }
   else if ( hour < 8 && day == 1 ) { 
-    res.send('Dude, Blipsy has been open since noon.  Go and drink!');
+    var message = 'Dude, Blipsy has been open since noon.  Go and drink!';
   }
   else if ( ( hour >= 23 || hour < 8 ) && day > 0 ) {
-    res.send('Barcade is open! Time to drink.');
+    var message = 'Barcade is open! Time to drink.';
   }
   else if ( hour < 9 && hour >= 8 ) {
-    res.send('Barcade is about to close, fool.  Hurry up!');
+    var message = 'Barcade is about to close, fool.  Hurry up!';
   }
   else {
-    res.send('Barcade is closed.  You should drink to make yourself feel better.');
+    var message = 'Barcade is closed.  You should drink to make yourself feel better.';
   }
   console.log(now + hour + "day is " + day);
+  return message;
+});
+
+app.get('/', function(req, res){
+  res.render('index', { message: checkOpen() })
 });
 
 app.listen(process.env.PORT || 8001);
